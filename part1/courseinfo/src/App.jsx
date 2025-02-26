c342887d956ac37266449c47b0e8ecc939d8b6bf
iamshashanksharma <shashank16septd@gmail.com>
2025-02-26 22:13:55 +0530

    Added exercise 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props)=>{
+  console.log(props)
+  return(
+    <>
+      <h1>
+        {props.course}
+      </h1>
+    </>
+  )
+}
+const Content = (props)=>{
+  return(
+    <>
+    <p>
+    {props.parts} {props.exercises}
+    </p>
+     
+    </>
 
-function App() {
-  const [count, setCount] = useState(0)
+  )
+}
+const Total = (props) =>{
+  return(
+     <>
+     <p>
+       Number of Exercises {props.number}
+     </p>
+      
+     </>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Header course ={course}/>
+      <Content parts ={part1} exercises ={exercises1} />
+      <Content parts ={part2} exercises ={exercises2} />
+      <Content parts ={part3} exercises ={exercises3} />
+      <Total number ={exercises1+exercises2+exercises3} />
+    </div>
   )
 }
 
